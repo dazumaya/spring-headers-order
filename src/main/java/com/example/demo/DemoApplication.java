3c730112f79f70a0a26f86b9cf9489214c1c5e75
dazumaya <dazumaya@yahoo-corp.jp>
2021-05-30 19:47:53 +0900

    support jetty

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,37 @@
 package com.example.demo;
 
+import org.eclipse.jetty.http.HttpCompliance;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.ServerConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Component
+	public class DemoWebServerCustomizer implements WebServerFactoryCustomizer<JettyServletWebServerFactory> {
+
+		@Override
+		public void customize(JettyServletWebServerFactory factory) {
+			factory.addServerCustomizers(server -> {
+				for (Connector connector : server.getConnectors()) {
+					if (connector instanceof ServerConnector) {
+						HttpConnectionFactory connectionFactory = connector
+								.getConnectionFactory(HttpConnectionFactory.class);
+						connectionFactory.setHttpCompliance(HttpCompliance.LEGACY);
+					}
+				}
+			});
+		}
 	}
 
 }
